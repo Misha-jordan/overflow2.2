ad4fe5e8dc8e629350d54a60aec576a8d5e9821b
Пользователь <@LAPTOP-HUM566OL>
2024-02-27 23:16:01 +0300

    Добавьте файлы проекта.

---
+++ Header.h
@@ -0,0 +1,94 @@
+class Matrix {
+private:
+    std::vector<std::vector<int>> data;
+    unsigned int m;
+    unsigned int n;
+
+public:
+    Matrix(unsigned int rows, unsigned int cols) : m(rows), n(cols) {
+        data.resize(m, std::vector<int>(n, 0));
+    }
+
+    ~Matrix() {}
+
+    std::vector<int>& operator[](unsigned int index) {
+        return data[index];
+    }
+
+    void fillRandom() {
+        srand(static_cast<unsigned int>(time(0)));
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                data[i][j] = rand() % 10;
+            }
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+        for (unsigned int i = 0; i < matrix.m; ++i) {
+            for (unsigned int j = 0; j < matrix.n; ++j) {
+                os << matrix.data[i][j] << ' ';
+            }
+            os << '\n';
+        }
+        return os;
+    }
+
+    Matrix& operator+=(const Matrix& other) {
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                data[i][j] += other.data[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix operator+(const Matrix& other) const {
+        Matrix result(m, n);
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix& operator-=(const Matrix& other) {
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                data[i][j] -= other.data[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix operator-(const Matrix& other) const {
+        Matrix result(m, n);
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                result.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator*(const Matrix& other) const {
+        Matrix result(m, other.n);
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < other.n; ++j) {
+                for (unsigned int k = 0; k < n; ++k) {
+                    result.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    bool operator==(const Matrix& other) const {
+        return data == other.data;
+    }
+
+    bool operator!=(const Matrix& other) const {
+        return !(*this == other);
+    }
+};
---
+++ overflow2.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "Header.h"
+
+
+int main() {
+    Matrix mat1(3, 3);
+    Matrix mat2(3, 3);
+
+    mat1.fillRandom();
+    mat2.fillRandom();
+
+    std::cout << "Matrix 1:\n" << mat1 << "\n";
+    std::cout << "Matrix 2:\n" << mat2 << "\n";
+
+    Matrix sum = mat1 + mat2;
+    std::cout << "Sum of matrices:\n" << sum << "\n";
+
+    Matrix diff = mat1 - mat2;
+    std::cout << "Difference of matrices:\n" << diff << "\n";
+
+    Matrix mult = mat1 * mat2;
+    std::cout << "Product of matrices:\n" << mult << "\n";
+
+    std::cout << "Matrix 1 == Matrix 2: " << (mat1 == mat2) << "\n";
+    std::cout << "Matrix 1 != Matrix 2: " << (mat1 != mat2) << "\n";
+
+    return 0;
+}
